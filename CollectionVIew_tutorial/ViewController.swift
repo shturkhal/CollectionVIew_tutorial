1215ddaf8721d7e0d186281a97cf2aac7bfb73f6
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-14 19:43:06 +0300

    Initial Commit

---
+++ CollectionVIew_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionVIew_tutorial
+//
+//  Created by Іван Штурхаль on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
